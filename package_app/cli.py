a375a795f1b013c890e89b7363ebf450f959efbc
MoneyMachine <moneymachine@MoneyMachines-MacBook-Air.local>
2024-08-06 22:36:14 +0300

    добавление норм нейминга

--- package_app/cli.py
+++ package_app/cli.py
@@ -4,15 +4,15 @@ from .api import fetch_packages
 from .compare import compare_packages
 
 def main():
-    parser = argparse.ArgumentParser(description="Fetch and compare package data from two branches.")
-    parser.add_argument('--branch1', type=str, default="p10", help='First branch to fetch data for')
-    parser.add_argument('--branch2', type=str, default="sisyphus", help='Second branch to fetch data for')
+    parser = argparse.ArgumentParser(description="Получение и сравнение данных пакета из двух бранчей.")
+    parser.add_argument('--branch1', type=str, default="p10", help='Первая ветка')
+    parser.add_argument('--branch2', type=str, default="sisyphus", help='Вторая ветка')
     args = parser.parse_args()
 
     try:
         packages_branch1 = fetch_packages(args.branch1)
         packages_branch2 = fetch_packages(args.branch2)
-        comparison_results = compare_packages(packages_branch1, packages_branch2)
+        comparison_results = compare_packages(packages_branch1, args.branch1, packages_branch2, args.branch2)
         with open('comparison_results.json', 'w') as f:
             json.dump(comparison_results, f, indent=4)
     except Exception as e:
--- package_app/compare.py
+++ package_app/compare.py
@@ -1,18 +1,18 @@
 from packaging.version import parse
 
-def compare_packages(first_branch_data, second_branch_data):
+def compare_packages(first_branch_data, name_first_branch, second_branch_data, name_second_branch):
     packages_from_first_branch = {pkg['name']: pkg for pkg in first_branch_data['packages']}
     packages_from_second_branch = {pkg['name']: pkg for pkg in second_branch_data['packages']}
 
-    in_p10_not_sisyphus = set(packages_from_first_branch) - set(packages_from_second_branch)
-    in_sisyphus_not_p10 = set(packages_from_second_branch) - set(packages_from_first_branch)
+    in_first_not_second = set(packages_from_first_branch) - set(packages_from_second_branch)
+    in_second_not_first = set(packages_from_second_branch) - set(packages_from_first_branch)
 
     results_by_arch = {
         arch: {
-            'in_p10_not_sisyphus': [pkg for pkg in in_p10_not_sisyphus if p10_packages[pkg]['arch'] == arch],
-            'in_sisyphus_not_p10': [pkg for pkg in in_sisyphus_not_p10 if sisyphus_packages[pkg]['arch'] == arch],
+            f'in_{name_first_branch}_not_{name_second_branch}': [pkg for pkg in in_first_not_second if packages_from_first_branch[pkg]['arch'] == arch],
+            f'in_{name_second_branch}_not_{name_first_branch}': [pkg for pkg in in_second_not_first if packages_from_second_branch[pkg]['arch'] == arch],
         }
-        for arch in set(pkg['arch'] for pkg in p10_data['packages'] + sisyphus_data['packages'])
+        for arch in set(pkg['arch'] for pkg in first_branch_data['packages'] + second_branch_data['packages'])
     }
 
     return results_by_arch
